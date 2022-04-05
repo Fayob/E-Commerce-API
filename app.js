ffe6de3bd2c896aba07bafc7052601472260fb9e
= <adedejifavour6@gmail.com>
2022-04-05 21:43:40 +0400

    E-Commerce Set Up

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Server Listening on port ${port}...`);)
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
