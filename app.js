f8a7129d9c63c627065ef40d020de66e41e4629f
= <adedejifavour6@gmail.com>
2022-04-05 22:15:17 +0400

    Connect to Database

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+require("dotenv").config();
+
 // express
+const express = require("express");
+const app = express();
 
-const express = require("express")
-const app = express()
+// connect DB
+const connectDB = require("./db/connect");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-        app.listen(port, console.log(`Server Listening on port ${port}...`);)
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server Listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
