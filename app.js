98a7c9a420f4d88615db29d277dfafc3e2da96a6
= <adedejifavour6@gmail.com>
2022-04-05 22:35:37 +0400

    MiddleWare Added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("express-async-errors");
 
 // express
 const express = require("express");
@@ -6,6 +7,19 @@ const app = express();
 
 // connect DB
 const connectDB = require("./db/connect");
+
+// middleware
+const notFoundMW = require("./middleware/not_found");
+const errorHandlerMW = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.use(notFoundMW);
+app.use(errorHandlerMW);
 
 const port = process.env.PORT || 5000;
 
