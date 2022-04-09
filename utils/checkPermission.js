3d5c0b68fa799845eef2bb600faf4d3532407e2a
= <adedejifavour6@gmail.com>
2022-04-09 12:19:02 +0400

    Some Changes

--- utils/checkPermission.js
+++ utils/checkPermission.js
@@ -6,6 +6,7 @@ const checkPermissions = (requestUser, resourceUserId) => {
   // console.log(typeof resourceUserId);
 
   if (requestUser.role === "admin") return;
+
   if (requestUser.userId === resourceUserId.toString()) return;
   throw new CustomError.UnauthorizedError(
     "Not Authorized to Access this Route"
