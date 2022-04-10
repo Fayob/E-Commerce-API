470e86f5334206e1808a6fc29fe1f811b72a1f5f
= <adedejifavour6@gmail.com>
2022-04-10 19:55:06 +0400

    Create Review Functionality Added

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -1,5 +1,33 @@
+const Review = require("../models/reviewModel");
+const Product = require("../models/productModel");
+
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../error-handlers");
+const { checkPermissions } = require("../utils");
+
 const createReview = async (req, res) => {
-  res.send("create Review");
+  const { product: productId } = req.body;
+
+  const isProductValid = await Product.findOne({ _id: productId });
+
+  if (!isProductValid) {
+    throw new CustomError.NotFoundError(`No Product with id: ${productId}`);
+  }
+
+  const alreadyComment = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadyComment) {
+    throw new CustomError.BadRequestError(
+      "You already dropped a comment for this product"
+    );
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
   res.send("get all Reviews");
