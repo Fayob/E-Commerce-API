5be49eb2bed6f2b5814a2106cddf47cc2c9baaeb
= <adedejifavour6@gmail.com>
2022-04-12 19:02:54 +0400

    Aggegate pipeline starts

--- models/productModel.js
+++ models/productModel.js
@@ -58,6 +58,10 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    nomOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
